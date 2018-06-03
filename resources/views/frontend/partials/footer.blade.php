fd94a413d224789e799fdbaeedad5bb31f48d9eb
Marc-Antoine DANNER <dannerma@gmail.com>
2018-06-03 22:00:38 +0200

    df

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <ul class="footer-links">
             <li><a href="{{ route('home') }}">Accueil</a></li>
-            <li><a href="{{ route('about') }}">Qui sommes nous ?</a></li>
+            
             <li><a href="{{ route('contact') }}">Contact</a></li>
 
         </ul>
