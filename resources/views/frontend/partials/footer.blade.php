3a0d3c0546f1264aebf2fe94349d8a8544d9aa76
Marc-Antoine DANNER <dannerma@gmail.com>
2018-06-03 17:42:23 +0200

    footer front

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="footer bg-dark">
     <div class="container">
         <ul class="footer-links">
-            <li><a href="{{ route('home') }}">@lang('labels.frontend.titles.home')</a></li>
-            <li><a href="{{ route('about') }}">@lang('labels.frontend.titles.about')</a></li>
-            <li><a href="{{ route('contact') }}">@lang('labels.frontend.titles.contact')</a></li>
-            <li><a href="{{ route('legal-mentions') }}">@lang('labels.frontend.titles.legal_mentions')</a></li>
+            <li><a href="{{ route('home') }}">Accueil</a></li>
+            <li><a href="{{ route('about') }}">Qui sommes nous ?</a></li>
+            <li><a href="{{ route('contact') }}">Contact</a></li>
+
         </ul>
-        <p>Code licensed <a href="" target="_blank" rel="license">MIT</a></p>
+        <p>Projet IG3 <a href="https://www.facebook.com/marcantoine.danner" target="_blank" rel="license">Marc-Antoine DANNER</a></p>
     </div>
 </footer>
