41647747b82b6622630d97fa95f7bad88366b808
Marc-Antoine DANNER <dannerma@gmail.com>
2018-06-03 15:35:39 +0200

    home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -13,21 +13,12 @@
 @section('content')
     <div class="card mb-3">
         <div class="card-header">
-            @lang('labels.frontend.titles.home')
+            Accueil
         </div>
 
         <div class="card-body">
             <p>
-                Nulla quis lorem ut libero malesuada feugiat. Sed porttitor lectus nibh. Vivamus suscipit tortor
-                eget felis porttitor volutpat. Nulla porttitor accumsan tincidunt. Vivamus suscipit tortor eget
-                felis porttitor volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-                posuere
-                cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
-                Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Curabitur non nulla sit amet
-                nisl
-                tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at
-                sem.
-                Quisque velit nisi, pretium ut lacinia in, elementum id enim.
+               Bienvenue sur le site de squash de la ligue occitanie
             </p>
         </div>
     </div>
