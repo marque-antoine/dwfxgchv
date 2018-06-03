90ad306b286376613f66d3f058550a75c6f4c606
Marc-Antoine DANNER <dannerma@gmail.com>
2018-06-03 16:30:13 +0200

    .

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -6,6 +6,6 @@
             <li><a href="{{ route('contact') }}">@lang('labels.frontend.titles.contact')</a></li>
             <li><a href="{{ route('legal-mentions') }}">@lang('labels.frontend.titles.legal_mentions')</a></li>
         </ul>
-        <p>Code licensed <a href="https://adr1enbe4udou1n.mit-license.org" target="_blank" rel="license">MIT</a></p>
+        <p>Code licensed <a href="" target="_blank" rel="license">MIT</a></p>
     </div>
 </footer>
